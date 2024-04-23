67d5d81ce742fe47d745af191be7f58767747606
Danila <danikk066@gmail.com>
2024-04-24 01:23:01 +0300

    feat: added registration

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,26 +1,53 @@
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useNavigate } from 'react-router-dom';
 
 import { SignUpForm } from '@/Components/SignUpForm';
 import twitterLogo from '@/assets/images/twitter-logo-4 1.png';
 import { LogoContainer, Wrapper } from '@/mixins/formStyleMixins';
 import { TypeSignup, signupSchema } from '@/utils/signUpFormSchema';
+import { registerNewUser } from '@/utils/registerNewUser';
+import { getBirthday } from '@/utils/getBirthday';
+import { RoutesEnum } from '@/constants/routesEnum';
+
+interface SignUpFormProps {
+  name: string;
+  phone: string;
+  email: string;
+  password: string;
+  month: string;
+  day: string;
+  year: string;
+}
 
 export const SignInPage = () => {
   const [disables, setDisabled] = useState<boolean>(false);
   const { reset } = useForm<TypeSignup>({ resolver: zodResolver(signupSchema) });
+  const navigate = useNavigate();
 
-  const onSubmit: SubmitHandler<TypeSignup> = (data) => {
+  const onSubmit: SubmitHandler<TypeSignup> = async ({
+    name,
+    phone,
+    email,
+    password,
+    month,
+    day,
+    year,
+  }: SignUpFormProps) => {
+    const birthday = getBirthday(Number(year), month, Number(day));
+
     try {
       setDisabled(true);
-      // eslint-disable-next-line no-console
-      console.log(data);
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { userData } = await registerNewUser(name, phone, email, password, birthday);
     } catch (error) {
       console.error(error);
     } finally {
       setDisabled(false);
 
+      navigate(RoutesEnum.Home);
       reset();
     }
   };
--- src/utils/registerNewUser.ts
+++ src/utils/registerNewUser.ts
@@ -3,15 +3,13 @@ import { addDoc, collection } from 'firebase/firestore';
 import { db, register } from '@/firebase';
 import { Collections } from '@/constants/collections';
 
-interface NewUserProps {
-  name: string;
-  phone: string;
-  email: string;
-  password: string;
-  birthDay: string;
-}
-
-export const registerNewUser = async ({ name, phone, email, password, birthDay }: NewUserProps) => {
+export const registerNewUser = async (
+  name: string,
+  phone: string,
+  email: string,
+  password: string,
+  birthDay: string,
+) => {
   const userCredentials = await register(email, password);
   const token = await userCredentials.user.getIdToken();
   const newUserData = {
