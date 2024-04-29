d27f4b1fa63646b8c20b861d63adab16407c115e
Danila <danikk066@gmail.com>
2024-04-30 01:16:12 +0300

    feat: added tweet page

--- src/pages/Tweet/index.tsx
+++ src/pages/Tweet/index.tsx
@@ -1,1 +1,43 @@
-export const TweetPage = () => <h1>This is Tweet page</h1>;
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { doc, getDoc } from 'firebase/firestore';
+
+import { Tweet } from '@/Components/Tweet';
+import { db } from '@/firebase';
+import { Collections } from '@/constants/collections';
+import { TweetProps } from '@/types/tweets';
+import { useAppSelector } from '@/hooks/redux';
+import { selectUser } from '@/store/selectors';
+
+export const TweetPage = () => {
+  const [tweet, setTweet] = useState<TweetProps | null>(null);
+  const { email } = useAppSelector(selectUser);
+  const { id } = useParams();
+
+  useEffect(() => {
+    // eslint-disable-next-line consistent-return
+    const getTweet = async () => {
+      if (!id) return null;
+      const tweetDoc = doc(db, Collections.Posts, id);
+      const tweetSnapshot = await getDoc(tweetDoc);
+
+      if (tweetSnapshot.exists()) {
+        setTweet(tweetSnapshot.data() as TweetProps);
+      } else {
+        console.error('There is no tweet with that id');
+      }
+    };
+
+    getTweet();
+  }, [id]);
+
+  return tweet ? (
+    <Tweet
+      id={id!}
+      tweet={tweet}
+      myEmail={email}
+    />
+  ) : (
+    <>Loading...</>
+  );
+};
