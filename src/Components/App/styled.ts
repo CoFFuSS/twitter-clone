c5953ba0f45be8d90d9dc277cc725326db77f838
Danila <danikk066@gmail.com>
2024-05-05 14:56:04 +0300

    t push -fix: padding in styles

--- src/Components/App/styled.ts
+++ src/Components/App/styled.ts
@@ -1,24 +1,24 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-body{
-    overflow-x: hidden;
-    box-sizing:border-box;
-    margin: 0;
-    padding: 0;
 
+body{
+  overflow-x: hidden;
+  box-sizing:border-box;
+  margin: 0;
+  padding: 0;
 }
 
 
 :root {
-	align-content: center;
+  align-content: center;
 
-	max-width: ${({ theme }) => theme.spacing(1920)};
-	margin: 0 auto;
+  max-width: ${({ theme }) => theme.spacing(1920)};
+  margin: 0 auto;
 
-	font-family: Roboto, sans-serif;
+  font-family: Roboto, sans-serif;
 
-	background-color: ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.primary};
 }
 
 .blur {
